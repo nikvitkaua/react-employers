dfc979fbfeea8b15176849a54c247dcd227b3d90
Mykyta Paiuka <nikvitka.it@gmail.com>
2024-01-29 23:37:49 +0200

    create layout of components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React, {StrictMode} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './components/app/app'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StrictMode>
-  </StrictMode>
+  <App/>
 );
 
